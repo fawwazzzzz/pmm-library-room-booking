ab3b551f6887858dcc4f8f3f2975864e3baf86d9
fawwaz <muhdfawwaz18@gmail.com>
2024-03-21 15:34:13 +0800

    add js to store checkin & checkout data in database

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -12,5 +14,26 @@ class MainController extends Controller
 
     public function details() {
         return view('forms.form-details');
+    }
+
+    public function checkBetween(Request $request) {
+
+        $request->validate([
+            'checkin' => 'required|date',
+            'checkout' => 'required|date|after:start_time',
+        ]);
+
+        // Find available rooms for the requested time slot
+        $availableRooms = Reservation::whereDoesntHave('reservations', function ($query) use ($request) {
+            $query->where('checkin', '<', $request->input('checkout'))
+                ->where('checkout', '>', $request->input('checkin'));
+        })->get();
+
+        if ($availableRooms->isEmpty()) {
+            return response()->json(['message' => 'No rooms available for the requested time slot.'], 422);
+        }
+
+        // If rooms are available, return them along with a success message
+        return response()->json(['available_rooms' => $availableRooms, 'message' => 'Rooms available.'], 200);
     }
 }
