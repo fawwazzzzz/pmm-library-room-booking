8c9ab8faf9c6ce57b5eefd2a2c74978603b098b9
fawwaz <muhdfawwaz18@gmail.com>
2024-04-01 13:34:17 +0800

    add migration jabatan and program

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(UserSeeder::class);
         $this->call(RoomSeeder::class);
+        $this->call(JabatanSeeder::class);
+        $this->call(ProgramSeeder::class);
     }
 }
