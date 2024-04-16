7fe9ec0fb40a873281dcce99dbaf87d30735376a
MuhdDanishs <danishfadillah65@gmail.com>
2024-04-16 11:29:12 +0800

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,43 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MainController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+// Routes accessible to authenticated users only
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Users';
+    });
+
+    Route::get('/admin', [MainController::class, 'admin'])->name('admin');
+
+    Route::get('/admin/tempahan', [AdminController::class, 'index'])->name('admin-tempahan');
+
+    Route::get('/admin/tempahan-pelajar-list', [AdminController::class, 'tempahanPelajarList'])->name('tempahan.pelajar-list');
+
+    Route::get('/admin/tempahan-pensyarah-list', [AdminController::class, 'tempahanPensyarahList'])->name('tempahan.pensyarah-list');
+
+    Route::post('/process-data', [MainController::class, 'checkBetween']);
+
+    Route::post('/insert-time', [MainController::class, 'insertTime'])->name('time');
+
+    Route::post('/details', [MainController::class, 'insertDetails'])->name('details');
+
+    Route::get('/delete-available/{id}', [MainController::class, 'deleteTime']);
+});
+
+// Routes accessible to all users
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Form Page
 
 Route::get('/form-available', [MainController::class, 'availablePage']);
 
@@ -21,22 +45,5 @@ Route::get('/form-details', [MainController::class, 'detailsPage']);
 
 Route::get('/form-result', [MainController::class, 'result']);
 
-// Admin
-
-Route::get('/admin', [MainController::class, 'admin'])->name('admin');
-
-Route::get('admin/tempahan', [AdminController::class, 'index'])->name('admin-tempahan');
-
-Route::get('admin/tempahan-pelajar-list', [AdminController::class, 'tempahanPelajarList'])->name('tempahan.pelajar-list');
-
-Route::get('admin/tempahan-pensyarah-list', [AdminController::class, 'tempahanPensyarahList'])->name('tempahan.pensyarah-list');
-
-// Data Process
-
-Route::post('/process-data', [MainController::class, 'checkBetween']);
-
-Route::post('/insert-time', [MainController::class, 'insertTime'])->name('time');
-
-Route::post('/details', [MainController::class, 'insertDetails'])->name('details');
-
-Route::get('/delete-available/{id}', [MainController::class, 'deleteTime']);
+// Authentication routes
+Auth::routes();
