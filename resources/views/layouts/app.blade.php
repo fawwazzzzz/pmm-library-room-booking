9d5587ced43c863dcfa1e8442a257b1b3b097248
fawwaz <muhdfawwaz18@gmail.com>
2024-04-19 11:30:04 +0800

    add class toggle in admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/bootstrap.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://code.jquery.com/jquery.min.js"></script>
     {{-- <script src="{{ asset('assets/js/bootstrap.js') }}"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/@floating-ui/core@1.6.0"></script>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/sass/app.scss",
-                "resources/js/app.js",
-                "resources/js/bootstrap.js",
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
