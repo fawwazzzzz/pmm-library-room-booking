76b4299b069eaf2c68e7f75f230fcb4cb57e898d
fawwazzzzz <muhdfawwaz18@gmail.com>
2024-08-27 18:14:33 +0800

    add record in month report

--- resources/views/admin/admin-month-dashboard.blade.php
+++ resources/views/admin/admin-month-dashboard.blade.php
@@ -17,13 +17,6 @@
     <div class="pagetitle d-flex justify-content-between align-items-center">
         <h2>{{ $month }}</h2>
         <i class="bi bi-list toggle-sidebar-btn d-block d-xl-none"></i>
-
-              {{-- <nav>
-                  <ol class="breadcrumb">
-                      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                      <li class="breadcrumb-item active">Dashboard</li>
-                  </ol>
-              </nav> --}}
     </div>
           <!-- End Page Title -->
 
@@ -126,6 +119,61 @@
                                 <h5 class="card-title">Masa Kerap Ditempah <span>| {{ $month }}</span></h5>
 
                                 <div id="reportsChart"></div> {{-- Start Program Chart --}}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="pagetitle d-flex justify-content-between align-items-center">
+                        <h2>Rekod</h2>
+                        <i class="bi bi-list toggle-sidebar-btn d-block d-xl-none"></i>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Pelajar <span>| {{ $month }}</span></h5>
+                                <div class="mt-4">
+                                    <div class="table-responsive">
+                                        <table class="table data-table-pelajar table-striped table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th style="width: 5%;">No Tempahan</th>
+                                                    <th>Nama Pengguna</th>
+                                                    <th>Tarikh</th>
+                                                    <th>Program</th>
+                                                    <th>No Matrik</th>
+                                                    <th>No Bilik</th>
+                                                    <th>(checkin)</th>
+                                                    <th>(checkout)</th>
+                                                </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Pensyarah <span>| {{ $month }}</span></h5>
+                                <div class="mt-4">
+                                    <div class="table-responsive">
+                                        <table class="table data-table-pensyarah table-striped table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th style="width: 5%;">No Tempahan</th>
+                                                    <th>Nama Pengguna</th>
+                                                    <th>Tarikh</th>
+                                                    <th>Jabatan</th>
+                                                    <th>No Bilik</th>
+                                                    <th>(checkin)</th>
+                                                    <th>(checkout)</th>
+                                                </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -334,8 +382,48 @@
                 }
             }
         });
-
     });
+
+    $(function () {
+        var tablePelajar = $('.data-table-pelajar').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('tempahan.pelajar-list') }}",
+            columns: [
+                // { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
+                { data: 'id', name: 'id'},
+                { data: 'namaPengguna', name: 'namaPengguna' },
+                { data: 'date', name: 'date', searchable: false },
+                { data: 'program', name: 'program', searchable: false },
+                { data: 'noMatrik', name: 'noMatrik', searchable: false },
+                { data: 'noBilik', name: 'noBilik', searchable: false },
+                { data: 'checkin', name: 'checkin', searchable: false },
+                { data: 'checkout', name: 'checkout', searchable: false }
+            ],
+            columnDefs: [
+                { targets: [0, 1, 2, 3, 4, 5, 6, 7], className: "text-start" },
+            ]
+        });
+        
+        var tablePensyarah = $('.data-table-pensyarah').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('tempahan.pensyarah-list') }}",
+            columns: [
+                // { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
+                { data: 'id', name: 'id'},
+                { data: 'namaPengguna', name: 'namaPengguna' },
+                { data: 'date', name: 'date' , searchable: false},
+                { data: 'jabatan', name: 'jabatan', searchable: false },
+                { data: 'noBilik', name: 'noBilik', searchable: false },
+                { data: 'checkin', name: 'checkin', searchable: false },
+                { data: 'checkout', name: 'checkout', searchable: false }
+            ],
+            columnDefs: [
+                { targets: [0, 1, 2, 3, 4, 5, 6], className: "text-start" },
+            ]
+        })
+    })
 
     
 </script>    
